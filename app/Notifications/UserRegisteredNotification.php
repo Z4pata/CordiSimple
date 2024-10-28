43ad8971f9f3c72e9068324050fc335558374117
Jhon-Develop <asprillajhon73@gmail.com>
2024-10-28 12:47:14 -0500

    A fix is made to the registry by adding some new functionalities

--- app/Notifications/UserRegisteredNotification.php
+++ app/Notifications/UserRegisteredNotification.php
@@ -6,17 +6,22 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Facades\URL;
+use App\Models\User;
 
 class UserRegisteredNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
+    protected $user;
+
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-
+        $this->user = $user;
     }
 
     /**
@@ -38,12 +43,21 @@ class UserRegisteredNotification extends Notification implements ShouldQueue
      */
     public function toMail($notifiable): MailMessage
     {
+        $verificationUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(15),
+            ['id' => $notifiable->id, 'hash' => sha1($notifiable->email)]
+        );
+    
         return (new MailMessage)
             ->subject('Registration Successful')
             ->greeting("Hello, {$notifiable->name}")
             ->line('Thank you for registering with us!')
             ->line('We are excited to have you on board.')
-            ->action('Visit our website', url('/'))
+            ->action('Verify your account', route('verification.verify', [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]))
             ->line("If you have any questions, please don’t hesitate to contact us.")
             ->from('cordisimpleriwi@gmail.com', 'CordiSimple');
     }
