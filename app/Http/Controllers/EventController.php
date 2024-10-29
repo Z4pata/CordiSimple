559c9c5a636a76c533a594fef3deadf084474186
medi77na <medinastevenurregodavid@gmail.com>
2024-10-29 07:31:18 -0500

    feat: The method is performed as requested

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -36,7 +36,7 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        //
+        return view('events.show', compact('event'));
     }
 
     /**
