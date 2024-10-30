db7d473908dc6ff8232f3210fcb2e03db53ea7ed
medi77na <medinastevenurregodavid@gmail.com>
2024-10-30 18:16:02 -0500

    "Added HasFactory trait, defined fillable fields, and implemented relationships, scopes, accessors, and mutators in Event model."

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,87 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    /*
+    |--------------------------------------------------------------------------
+    | GLOBAL VARIABLES
+    |--------------------------------------------------------------------------
+    */
+
+    // protected $table = 'events';
+    // protected $primaryKey = 'id';
+    // public $timestamps = true;
+    // protected $guarded = [];
+
+    protected $fillable = [
+        'name',
+        'description',
+        'date',
+        'time',
+        'location',
+        'max_seats',
+        'available_seats',
+        'status',
+        'admin_id'
+    ];
+
+    // protected $hidden = [
+    // ];
+    // protected $dates = [];
+
+    /*
+    |--------------------------------------------------------------------------
+    | FUNCTIONS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+
+    public function user()
+    {
+        return $this->belongsTo(related: User::class);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | SCOPES
+    |--------------------------------------------------------------------------
+    */
+
+    public function scopeByEvent($query, $eventId)
+    {
+        return $query->where('event_id', $eventId);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESORS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | MUTATORS
+    |--------------------------------------------------------------------------
+    */
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtolower($value);
+    }
+
+    public function setStatusAttribute($value)
+    {
+        $this->attributes['status'] = strtolower($value);
+    }
 }
