1f1fffa101aece553cc28ec8cf545eb328b7010e
user <>
2024-11-05 08:03:18 -0500

    Feat: Redirect to dashboard admin if the logged user is admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
         @if (Route::has('login'))
             <nav class="mx-12 flex justify-center col-2">
                 @auth
-                    <a href="{{ url('/dashboard') }}"
+                    <a href="{{ Auth::user()->isAdmin() ? url('/admin/dashboard') : url('/dashboard')}}"
                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">Dashboard</a>
                 @else
                     <a href="{{ route('login') }}"
