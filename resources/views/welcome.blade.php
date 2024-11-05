d90a1b73b86c0faccf89ae338d71a61397d12bfa
user <>
2024-11-05 11:06:49 -0500

    Fest: Add cursor pointer on the log out button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,7 @@
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
                             <input type="submit"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                class="cursor-pointer rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                 value="log out">
                         </form>
                     @endif
