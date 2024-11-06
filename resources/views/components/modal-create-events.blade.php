6f37bb1c844f1f8e178ac5a0744c2871227eaf2f
medi77na <medinastevenurregodavid@gmail.com>
2024-11-06 06:57:13 -0500

    "Updated modal-create-events component to accept title prop, added title prop to modal-create-events instance in events/index, and made minor styling changes to events/index"

--- resources/views/components/modal-create-events.blade.php
+++ resources/views/components/modal-create-events.blade.php
@@ -1,8 +1,10 @@
+@props(['title'])
+
 <div id="modal-create" class="overlay modal overlay-open:opacity-100 hidden" role="dialog" tabindex="-1">
     <div class="modal-dialog overlay-open:opacity-100 modal-dialog-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title">Create New Event</h3>
+                <h3 class="modal-title">{{ $title ?? 'Nombre no disponible' }}</h3>
                 <button type="button" class="btn btn-text btn-circle btn-sm absolute end-3 top-3" aria-label="Close"
                     data-overlay="#modal-create">
                     <span class="icon-[tabler--x] size-4"></span>
@@ -48,10 +50,10 @@
                                 class="input input-filled peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
                                 required>
                                 <option value="" disabled selected>Select Status</option>
-                                <option value="available">Available</option>
-                                <option value="cancelled">Cancelled</option>
-                                <option value="finished">Finished</option>
-                                <option value="not available">Not Available</option>
+                                <option class="text-orange-600" value="available">Available</option>
+                                <option class="text-orange-600" value="cancelled">Cancelled</option>
+                                <option class="text-orange-600" value="finished">Finished</option>
+                                <option class="text-orange-600" value="not available">Not Available</option>
                             </select>
                             <span
                                 class="absolute start-0 top-2 -translate-y-1/2 text-xs text-orange-600 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.event')
 
 @section('content')
-<div class="my-8">
-    <h1 class="text-center text-5xl font-semibold font-mono  text-orange-600/75">EVENTS</h1>
-</div>
+    <div class="my-8">
+        <h1 class="text-center text-5xl font-semibold font-mono  text-orange-600/75">EVENTS</h1>
+    </div>
     {{-- create event button --}}
     <div class="flex justify-end m-6">
-        <button type="button" class="btn btn-gradient btn-warning" aria-haspopup="dialog" aria-expanded="false" aria-controls="modal-create" data-overlay="#modal-create">
+        <button type="button" class="btn btn-gradient btn-warning" aria-haspopup="dialog" aria-expanded="false"
+            aria-controls="modal-create" data-overlay="#modal-create">
             <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24">
                 <g fill="none">
                     <path
@@ -16,7 +17,7 @@
                 </g>
             </svg> New Event
         </button>
-        <x-modal-create-events/>
+        <x-modal-create-events title="Create New Event" />
     </div>
     {{-- Table to display the events --}}
     <div class="card w-full">
@@ -39,6 +40,7 @@
                             <td>{{ $event->date }}</td>
                             <td class="text-nowrap">{{ $event->time }}</td>
                             <td class="text-nowrap">{{ $event->location }}</td>
+
                             {{-- Calls the component to define the status. --}}
                             <td><x-status-events :status="$event->status" /></td>
 
@@ -47,14 +49,23 @@
 
                             {{-- Calls the component to confirm event deletion and sends the object as a parameter. --}}
                             <x-modal-delete-events :event="$event" />
+
+                            {{-- Calls the component to edit event and sends the object as a parameter. --}}
+                            <x-modal-create-events :event="$event" />
+
                             <td>
-                                <button class="btn btn-circle btn-text btn-sm" aria-label="Action button"><span
-                                        class="icon-[tabler--edit]"></span></button>
+                                {{-- <button type="button" title="Edit Event" class="btn btn-circle btn-text btn-sm"
+                                    aria-label="Action button" aria-haspopup="dialog" aria-expanded="false"
+                                    aria-controls="modal-edit{{ $event }}"
+                                    data-overlay="#modal-edit{{ $event }}"><span
+                                        class="icon-[tabler--edit]"></span></button> --}}
+
                                 <button type="button" title="Delete Event" class="btn btn-circle btn-text btn-sm"
                                     aria-label="Action button" aria-haspopup="dialog" aria-expanded="false"
                                     aria-controls="modal-delete{{ $event->id }}"
                                     data-overlay="#modal-delete{{ $event->id }}"><span
                                         class="icon-[tabler--trash]"></span></button>
+
                                 <button type="button" title="Show Details" class="btn btn-circle btn-text btn-sm"
                                     aria-label="Action button" aria-haspopup="dialog" aria-expanded="false"
                                     aria-controls="modal-show{{ $event->id }}"
